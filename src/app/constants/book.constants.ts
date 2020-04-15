3a9898d8e15d0999d251e610d1c4b53fb55cb9c0
Adam Callanan <a.callanan@sbcglobal.net>
2020-04-15 13:35:53 -0500

    working version of mike mulligan

--- src/app/constants/book.constants.ts
+++ src/app/constants/book.constants.ts
@@ -1,25 +1,27 @@
 // tslint:disable: max-line-length
 import { Book } from "../models/Book.model";
 
+export const MIKE_MULLIGAN: Book = {
+  id: "1",
+  title: "Mike Mulligan and his Steam Shovel",
+  details: `The first book for the kids book debate club is
+  Mike Mulligan and his Steam Shovel. You can watch someone
+  read the book out loud (with the illustrations!) below.`,
+  debateQuestion: `The book claims that Mike Mulligan loves Mary Anne, but is their love related to the working relationship they have? Or is their love deep and noble?`,
+  date: "Sunday, April 19, 2020",
+  sides: [
+    {
+      name: "TRUE LOVE",
+      details: "Mike Mulligan has a true, deep love and respect for Mary Anne that goes far beyond their working relationship.",
+    },
+    {
+      name: "BASE LOVE",
+      details: "Mike Mulligan has a base love for Mary Anne, that does not go far past their working relationship.",
+    },
+  ],
+  youtubeLink: "NQjHJKNyoUE",
+};
+
 export const BOOK_LIST: Book[] = [
-  {
-    id: "1",
-    title: "Mike Mulligan and his Steam Shovel",
-    details: `The first book for the kids book debate club is
-    Mike Mulligan and his Steam Shovel. You can watch someone
-    read the book out loud (with the illustrations!) below.`,
-    debateQuestion: `We will be debating whether X or Y. You can come into the debate with a side picked, or start out neutral. Both sides will have a captain who will make a presentation for their side. During the debate everyone but the captain can switch sides. Whichever side has most people at the end wins!`,
-    date: "?",
-    sides: [
-      {
-        name: "",
-        details: "",
-      },
-      {
-        name: "",
-        details: "",
-      },
-    ],
-    youtubeLink: "NQjHJKNyoUE",
-  }
+  MIKE_MULLIGAN
 ];
